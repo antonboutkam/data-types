0afa4b0606c0e735b29aaff665f6da3c039bfe9f
Anton Boutkam <anton@nui-boutkam.nl>
2020-12-08 13:20:13 +0100

    [+] Added validation for DnsName [+] Added DnsNameCollection and Iterator [+] Added some tests

---
+++ src/Types/Type/DnsName/Iterator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hurah\Types\Type\DnsName;
+
+use Hurah\Types\Type\DnsName;
+use Hurah\Types\Type\DnsNameCollection;
+use Hurah\Types\Util\BaseIterator;
+
+class Iterator extends BaseIterator {
+
+    /***
+     * PathCollection constructor.
+     *
+     * @param DnsNameCollection $oDnsNameCollection
+     */
+    function __construct(DnsNameCollection $oDnsNameCollection) {
+        $this->array = $oDnsNameCollection->toArray();
+        $this->position = 0;
+    }
+
+    public function current(): DnsName {
+        return $this->array[$this->position];
+    }
+}
---
+++ src/Types/Type/DnsNameCollection.php
@@ -0,0 +1,65 @@
+<?php
+namespace Hurah\Types\Type;
+
+use Hurah\Types\Exception\InvalidArgumentException;
+use Hurah\Types\Type\DnsName\Iterator;
+
+class DnsNameCollection extends AbstractDataType implements IComplexDataType {
+
+    private int $position = 0;
+
+    /***
+     * PathCollection constructor.
+     *
+     * @param array $mValues - An array of strings[] or Path[], internally will all be converted to Paths.
+     */
+    function __construct($mValues = null) {
+        $this->position = 0;
+
+        parent::__construct([]);
+
+        if (is_array($mValues)) {
+            foreach ($mValues as $mValue) {
+                $this->add($mValue);
+            }
+        }
+    }
+
+    /**
+     * @param $mValue
+     * @throws InvalidArgumentException
+     */
+    function add($mValue): void {
+        if (is_string($mValue)) {
+            $objectItem = DnsName::fromString($mValue);
+        } else {
+            if ($mValue instanceof DnsName) {
+                $objectItem = $mValue;
+            } else {
+                throw new InvalidArgumentException("Only DnsName and strings are valid arguments.");
+            }
+        }
+
+        $aValues = $this->getValue();
+        $aValues[] = $objectItem;
+        $this->setValue($aValues);
+    }
+
+    function toArray(): array {
+        return $this->getValue();
+    }
+
+    function getIterator(): Iterator
+    {
+        return new Iterator($this);
+    }
+
+    public function __toString(): string {
+        $aOut = [];
+        foreach ($this->getValue() as $oPath) {
+            $aOut[] = (string)$oPath;
+        }
+        return join(',', $aOut);
+    }
+
+}
