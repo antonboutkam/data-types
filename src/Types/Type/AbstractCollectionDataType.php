bdcf356109f0b71201ce671b59d1afb82808c042
Anton Boutkam <anton@nui-boutkam.nl>
2021-03-22 10:06:40 +0100

    AbstractCollectionDataType implements ArrayAccess

--- src/Types/Type/AbstractCollectionDataType.php
+++ src/Types/Type/AbstractCollectionDataType.php
@@ -2,28 +2,57 @@
 
 namespace Hurah\Types\Type;
 
+use ArrayAccess;
 use Iterator;
 
-abstract class AbstractCollectionDataType extends AbstractDataType implements IGenericDataType, Iterator
+abstract class AbstractCollectionDataType extends AbstractDataType implements IGenericDataType, Iterator, ArrayAccess
 {
     protected int $position = 0;
     protected array $array;
 
     abstract public function current();
 
-    public function rewind() {
+    public function rewind()
+    {
         $this->position = 0;
     }
 
-    public function key() {
+    public function key():int
+    {
         return $this->position;
     }
 
-    public function next() {
+    public function next()
+    {
         ++$this->position;
     }
 
-    public function valid() {
+    public function valid():bool
+    {
         return isset($this->array[$this->position]);
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->array[] = $value;
+        } else {
+            $this->array[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset): bool
+    {
+        return isset($this->array[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->array[$offset]);
+    }
+
+    public function offsetGet($offset)
+    {
+        return isset($this->array[$offset]) ? $this->array[$offset] : null;
     }
 }
