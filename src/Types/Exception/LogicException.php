ac3f079cc543d3c54af37877b664be8bf5532007
Anton Boutkam <anton@nui-boutkam.nl>
2022-08-03 14:27:20 +0200

    + Improved message in exception

---
+++ src/Types/Exception/LogicException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Hurah\Types\Exception;
+
+use Exception;
+
+/**
+ * Class FileNotFoundException
+ * @package Hurah\Type\Exception
+ */
+final class LogicException extends Exception {
+
+}
