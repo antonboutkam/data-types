a153b4ffd5c026a861cf9c725c962eba01f3176a
Anton Boutkam <anton@nui-boutkam.nl>
2022-10-20 13:31:48 +0200

    Added Url Collection

---
+++ src/Types/Type/UrlCollection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Hurah\Types\Type;
+
+use Hurah\Types\Type\Http\Response;
+use InvalidArgumentException;
+
+class UrlCollection extends AbstractCollectionDataType
+{
+    public function add(Url $oUrl)
+    {
+        $this->array[] = $oUrl;
+    }
+
+    public function current(): Url
+    {
+        return $this->array[$this->position];
+    }
+}
