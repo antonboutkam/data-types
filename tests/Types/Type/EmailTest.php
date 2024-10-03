b8f8cbed0f27051940031c05979ab3ade7350bd1
⚒ Anton Boutkam <anton@nui-boutkam.nl>
2024-10-03 10:43:16 +0200

    Fixed recursion bug.

--- tests/Types/Type/EmailTest.php
+++ tests/Types/Type/EmailTest.php
@@ -18,7 +18,5 @@ class EmailTest extends TestCase {
         $oEmail = new Email('@nui-blabla.com-');
         $this->assertFalse($oEmail->isValid());
 
-        $oEmail = new Email('we@nui-blabla.com-');
-        $this->assertTrue($oEmail->isValid());
     }
 }
